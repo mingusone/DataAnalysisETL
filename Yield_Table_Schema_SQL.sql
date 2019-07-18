1d1c7eb0e0293218d8de449d52207875e42ce06e
MingUSone <6264548+mingusone@users.noreply.github.com>
2019-07-17 21:49:43 -0400

    Yield Stuff. Scrubbed from Treasury.gov, cleaned using python, pushed to database (see the .sql file)

---
+++ Yield_Table_Schema_SQL.sql
@@ -0,0 +1,17 @@
+CREATE TABLE yield (
+  Date VARCHAR(10) PRIMARY KEY,
+	Yield_1mo NUMERIC,
+	Yield_2mo NUMERIC,
+	Yield_3mo NUMERIC,
+	Yield_6mo NUMERIC,
+	Yield_1yr NUMERIC,
+	Yield_2yr NUMERIC,
+	Yield_3yr NUMERIC,
+	Yield_5yr NUMERIC, 
+	Yield_7yr NUMERIC,
+	Yield_10yr NUMERIC,
+	Yield_20yr NUMERIC,
+	Yield_30yr NUMERIC
+);
+
+--After this, Just import the CSV into the Database
